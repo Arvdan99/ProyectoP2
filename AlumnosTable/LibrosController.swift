ac9c46f3795fd15ac91c73b825eb6b78a7c0a1d4
Arvdan99 <71156318+Arvdan99@users.noreply.github.com>
2022-10-18 02:10:30 -0700

    Update LibrosController.swift

--- AlumnosTable/LibrosController.swift
+++ AlumnosTable/LibrosController.swift
@@ -23,6 +23,12 @@ class LibrosController: UIViewController, UITableViewDelegate, UITableViewDataSo
     var Ediciones9 : [Ediciones] = []
     var Ediciones10 : [Ediciones] = []
     var Ediciones11 : [Ediciones] = []
+    var Ediciones12 : [Ediciones] = []
+    var Ediciones13 : [Ediciones] = []
+    var Ediciones14 : [Ediciones] = []
+    var Ediciones15 : [Ediciones] = []
+    
+    
     
     //Altura de celda
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -109,24 +115,42 @@ class LibrosController: UIViewController, UITableViewDelegate, UITableViewDataSo
         Ediciones11.append(Ediciones(edicion: "1", editorial: "ALMUZARA", año: "2006"))
         Ediciones11.append(Ediciones(edicion: "2", editorial: "ALMUZARA", año: "2008"))
         Ediciones11.append(Ediciones(edicion: "CreateSpace", editorial: "Debolsillo", año: "2018"))
-            
+        
+        Ediciones12.append(Ediciones(edicion: "EUA", editorial: "Reynal&Hitchcock", año: "1944"))
+        Ediciones12.append(Ediciones(edicion: "Francia", editorial: "Gallimard", año: "1943"))
+        Ediciones12.append(Ediciones(edicion: "Argentina", editorial: "Emecé", año: "1954"))
+        
+        Ediciones13.append(Ediciones(edicion: "España", editorial: "TIKAL EDITORIAL", año: "2014"))
+        Ediciones13.append(Ediciones(edicion: "España", editorial: "Dojo", año: "2018"))
+        Ediciones13.append(Ediciones(edicion: "México", editorial: "GANDHI", año: "2020"))
+        
+        Ediciones14.append(Ediciones(edicion: "Santillana", editorial: "Loqueleo", año: "2014"))
+        Ediciones14.append(Ediciones(edicion: "Tapa dura", editorial: "Alfaguara", año: "2019"))
+        Ediciones14.append(Ediciones(edicion: "1", editorial: "Thienemanns", año: "1979"))
           
+        Ediciones15.append(Ediciones(edicion: "1", editorial: "SUMA", año: "2013"))
+        Ediciones15.append(Ediciones(edicion: "España", editorial: "PENGUIN", año: "2019"))
+        Ediciones15.append(Ediciones(edicion: "México", editorial: "Debolsillo", año: "2020"))
    
         
         
         
         Libros.append(Libro(nombre: "Harry Potter y la Piedra Filosofal", año: "1997", genero: "Fantasía", autor: "J.K Rowling", ediciones: Ediciones1, paginas: "256", encuadernacion: "Tapa dura/blanda", copiasv: "120 Millones", precio: "$299", imagen: "PiedraFilosofal", imagen2: "PiedraFilosofal1"))
-        Libros.append(Libro(nombre: "Harry Potter y la Cámara Secreta", año: "1998", genero: "Fantasía", autor: "J.K Rowling", ediciones: Ediciones2, paginas: "288", encuadernacion: "Tapa blanda", copiasv: "60 Milones", precio: "$649", imagen: "CamaraSecreta", imagen2: "CamaraSecreta2"))
+        Libros.append(Libro(nombre: "Harry Potter y la Cámara Secreta", año: "1998", genero: "Fantasía", autor: "J.K Rowling", ediciones: Ediciones2, paginas: "288", encuadernacion: "Tapa blanda", copiasv: "60 Millones", precio: "$649", imagen: "CamaraSecreta", imagen2: "CamaraSecreta2"))
         Libros.append(Libro(nombre: "Cementerio de animales", año: "1983", genero: "Terror", autor: "Stephen King", ediciones: Ediciones3, paginas: "488", encuadernacion: "Tapa blanda", copiasv: "40 Milones", precio: "$399", imagen: "CementerioA", imagen2: "CementerioA2"))
-        Libros.append(Libro(nombre: "El resplandor", año: "1977", genero: "Terror", autor: "Stephen King", ediciones: Ediciones4, paginas: "643", encuadernacion: "Tapa blanda", copiasv: "350 Milones", precio: "$499", imagen: "Resplandor", imagen2: "Resplandor2"))
+        Libros.append(Libro(nombre: "El resplandor", año: "1977", genero: "Terror", autor: "Stephen King", ediciones: Ediciones4, paginas: "643", encuadernacion: "Tapa blanda", copiasv: "350 Millones", precio: "$499", imagen: "Resplandor", imagen2: "Resplandor2"))
         Libros.append(Libro(nombre: "Marcar como no leído", año: "2018", genero: "Autobiografico", autor: "Zpu", ediciones: Ediciones5, paginas: "244", encuadernacion: "Tapa blanda", copiasv: "300 mil", precio: "$265", imagen: "NoLeido", imagen2: "NoleidoZpu"))
         Libros.append(Libro(nombre: "QUE. TE. JODAN.", año: "2018", genero: "Poesia", autor: "Soma", ediciones: Ediciones6, paginas: "120", encuadernacion: "Tapa blanda", copiasv: "200 mil", precio: "$300", imagen: "Soma", imagen2: "Soma2"))
         Libros.append(Libro(nombre: "Casino Royale", año: "1953", genero: "Espionaje", autor: "Ian Fleming", ediciones: Ediciones6, paginas: "120", encuadernacion: "Tapa blanda", copiasv: "200 mil", precio: "$300", imagen: "CasinoR1", imagen2: "CasinoR"))
         Libros.append(Libro(nombre: "Siete martes", año: "2021", genero: "Ficción", autor: "Chojin", ediciones: Ediciones7, paginas: "320", encuadernacion: "Tapa blanda", copiasv: "190 mil", precio: "$400", imagen: "SieteM", imagen2: "SieteM1"))
         Libros.append(Libro(nombre: "Las crónicas de Magnus Bane.", año: "2015", genero: "Fantasía", autor: "Cassandra Clare", ediciones: Ediciones8, paginas: "470", encuadernacion: "Tapa blanda", copiasv: "50 Millones", precio: "$449", imagen: "Magnus", imagen2: "Magnus1"))
         Libros.append(Libro(nombre: "1984.", año: "2015", genero: "Ficción", autor: "George Orwell", ediciones: Ediciones9, paginas: "368", encuadernacion: "Rustica", copiasv: "200 Millones", precio: "$299", imagen: "1984", imagen2: "1984b"))
-         Libros.append(Libro(nombre: "La teoría del todo.", año: "2010", genero: "Ciencia", autor: "Stephen Hawking", ediciones: Ediciones10, paginas: "152", encuadernacion: "Tapa Blanda", copiasv: "100 Millones", precio: "$323", imagen: "Teoria", imagen2: "Teoria1"))
+        Libros.append(Libro(nombre: "La teoría del todo.", año: "2010", genero: "Ciencia", autor: "Stephen Hawking", ediciones: Ediciones10, paginas: "152", encuadernacion: "Tapa Blanda", copiasv: "100 Millones", precio: "$323", imagen: "Teoria", imagen2: "Teoria1"))
         Libros.append(Libro(nombre: "Guerra Mundial Z.", año: "2006", genero: "Horror", autor: "Max Brooks", ediciones: Ediciones11, paginas: "464", encuadernacion: "Tapa Blanda", copiasv: "310 Millones", precio: "$599", imagen: "Guerra", imagen2: "Guerra1"))
+        Libros.append(Libro(nombre: "El principito", año: "1943", genero: "Novela", autor: "Saint-Exupéry", ediciones: Ediciones12, paginas: "92", encuadernacion: "Tapa Blanda", copiasv: "500 Millones", precio: "$237", imagen: "Guerra", imagen2: "Guerra1"))
+        Libros.append(Libro(nombre: "El arte de la guerra", año: "1772", genero: "No-ficción", autor: "Sun Tzu", ediciones: Ediciones13, paginas: "288", encuadernacion: "Tapa Blanda", copiasv: "20 Millones", precio: "$336", imagen: "Guerra", imagen2: "Guerra1"))
+        Libros.append(Libro(nombre: "La historia interminable", año: "1979", genero: "Fantasía", autor: "Michael Ende", ediciones: Ediciones14, paginas: "500", encuadernacion: "Tapa Blanda/Dura", copiasv: "10 Millones", precio: "$635", imagen: "Guerra", imagen2: "Guerra1"))
+        Libros.append(Libro(nombre: "Memento mori", año: "2013", genero: "Thriller", autor: "César Pérez", ediciones: Ediciones15, paginas: "531", encuadernacion: "Tapa Blanda", copiasv: "500 mil", precio: "$700", imagen: "Guerra", imagen2: "Guerra1"))
         
     }
     
